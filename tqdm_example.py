61ed81082d9f70af4f5bc6d8a7ceea6d47c85ffd
ggang.liu <304241378@qq.com>
2018-10-24 18:11:26 +0800

    Create tqdm_example.py

---
+++ tqdm_example.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello anaconda")
+from tqdm import tqdm, trange
+
+from time import sleep
+
+for i in tqdm(range(100)):
+    sleep(0.1)     
