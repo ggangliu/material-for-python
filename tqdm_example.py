a2914d534eb4bd13522561cd18668b073a7b3e2e
ggang.liu <304241378@qq.com>
2018-10-25 11:17:59 +0800

    Update tqdm_example.py

--- tqdm_example.py
+++ tqdm_example.py
@@ -5,10 +5,46 @@ Spyder Editor
 This is a temporary script file.
 """
 
-print("Hello anaconda")
 from tqdm import tqdm, trange
-
 from time import sleep
+import threading
 
-for i in tqdm(range(100)):
-    sleep(0.1)     
+lock = threading.RLock()
+con = threading.Condition()
+event = threading.Event()
+    
+def example3():
+    event.wait()
+    pbar = tqdm(["a", "b", "c", "d"], desc='customer')
+    for char in pbar:
+        sleep(1)
+
+def example2():
+    text = ""
+    for char in tqdm(["a", "b", "c", "d"], desc='generator'):
+        sleep(1)
+        text = text + char
+    #Notify example3 to run    
+    event.set()   
+    
+def example1():
+    for i in trange(100, desc='example1'):
+        sleep(0.1)
+    
+
+
+if __name__ == '__main__':
+
+    t1 = threading.Thread(target=example1) 
+    t2 = threading.Thread(target=example2) 
+    t1.setDaemon(False)
+    t2.setDaemon(False)
+    t1.start()
+    t2.start()
+    
+    ####################
+    t3 = threading.Thread(target=example3)
+    t3.setDaemon(False)
+    t3.start()
+    
+    print("main end")
